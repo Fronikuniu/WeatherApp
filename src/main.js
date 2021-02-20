4a186569b57ffc86d94173b3da55a43f35870236
Fronikuniu <Fronikuniu@gmail.com>
2021-02-20 16:17:01 +0100

    Finished v.1.0; visualized all weather info

--- src/main.js
+++ src/main.js
@@ -33,10 +33,12 @@ const setupListeners = () => {
 
 const onEnterSubmit = event => {
     if (event.key === 'Enter'){
+        fadeInOut();
         let query = viewElems.searchInput.value;
         getWeatherByCity(query)
         .then(data => {
             console.log(data);
+            displayWeatherData(data);
         });
     }
 }
@@ -47,9 +49,26 @@ const onClickSubmit = event => {
     getWeatherByCity(query)
     .then(data => {
         console.log(data);
-        switchView();
-        fadeInOut();
+        displayWeatherData(data);
     });
+}
+
+const displayWeatherData = data => {
+    switchView();
+    fadeInOut();
+
+    const weather = data.consolidated_weather[0];
+    viewElems.weatherCity.innerText = data.title;
+    viewElems.weatherIcon.src = `https://www.metaweather.com/static/img/weather/${weather.weather_state_abbr}.svg`;
+    viewElems.weatherIcon.alt = weather.weather_state_name;
+
+    const currentTemp = weather.the_temp.toFixed(2);
+    const maxTemp = weather.max_temp.toFixed(2);
+    const minTemp = weather.min_temp.toFixed(2);
+
+    viewElems.weatherCurrentTemp.innerText = `Current temperature: ${currentTemp}°C`;
+    viewElems.weatherMaxTemp.innerText = `Max temperature: ${maxTemp}°C`;
+    viewElems.weatherMinTemp.innerText = `Min temperature: ${minTemp}°C`;
 }
 
 const fadeInOut = () => {
